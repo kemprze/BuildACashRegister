68c303c6428a83316cbc0480896516a2452658c1
Przemyslaw Malec <malec.pkm@gmail.com>
2025-06-07 13:47:13 +0200

    starting to add logic

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+// defining element references
+
 const billList = document.querySelector(".bill-list")
+const fundsAvailable = document.querySelector(".available-funds")
+const cashPayment = Number(document.getElementById("cash").value.match(regex));
+const purchaseBtn = document.getElementById("purchase-btn")
+const statusMessage = document.getElementById("change-due")
+
+// regex to filter out non-payment-like values
+const regex = /\d+.\d{2}/gi
+
+// defining variables and constants 
 
 let price = 1.87;
 let cid = [
@@ -25,12 +36,45 @@ const billValues = {
   "ONE HUNDRED": 100
 }
 
+// defining functions
+
 const valueToBill = (element, billValues) => Math.floor(element[1] / billValues[element[0]]);
 
 const showCurrentCashState = (billList, billValues) => {
-  const {title, value} = cid;
-
-  cid.forEach((c) => {billList.innerHTML += `<li class="bill">${c[0]} - ${c[1]} -- ${valueToBill(c, billValues)} bills</li>`});
+  cid.forEach((c) => {billList.innerHTML += `<li class="bill"><b>${c[0]}</b> - $${c[1]} -- ${valueToBill(c, billValues)} bills</li>`});
 }
 
-showCurrentCashState(billList, billValues)
+const totalRegister = (cid) => {
+  let sum = 0;
+  cid.forEach((c) => sum += c[1]);
+
+  return Number(sum.toFixed(2));
+}
+
+const updateFunds = () => {
+  fundsAvailable.innerHTML += `<div class="funds">$${funds}</div>`
+}
+
+const enoughFundsInReg = () => funds >= cashPayment;
+
+const exactValue = () => funds == cashPayment;
+
+const enoughPayment = () => price <= cashPayment;
+
+const funds = totalRegister(cid);
+
+const updateRegisterStatus = () => {
+  if (!enoughFundsInReg()) {
+    statusMessage.innerText = "Status: INSUFFICIENT_FUNDS";
+  } else {
+    statusMessage.innerText = "Status: OPEN";
+  }
+  
+  if (exactValue()) {
+    statusMessage.innerText = "Status: CLOSED";
+  } 
+}
+
+showCurrentCashState(billList, billValues);
+updateFunds()
+updateRegisterStatus();
